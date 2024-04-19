140690481240a358ced36b97e6ab47cfd6b6ed69
Rajeev Kumar <rajeevku02@gmail.com>
2024-04-19 18:47:06 +0530

    Update

--- assets/js/head_script.js
+++ assets/js/head_script.js
@@ -1,11 +1,4 @@
 
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-1VD2W4SG2P");
-
 document.addEventListener("DOMContentLoaded", () => {
 
   const openNav = document.querySelector("#open-nav");
