412855b852fb0d5efd66206ad5c7381ed153675e
Rajeev Kumar <rajeevku02@gmail.com>
2024-04-19 14:00:33 +0530

    Update

---
+++ _next/static/mYcwkAxXA811Jh24LIPtK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblogs","\u002Fblogs\u002F[slug]","\u002Flegal\u002F[slug]","\u002Fresources\u002F[slug]","\u002Fresources\u002Fusecases","\u002Fvideos"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ assets/js/head_script.js
@@ -0,0 +1,53 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-1VD2W4SG2P");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const openNav = document.querySelector("#open-nav");
+  const closeNav = document.querySelector("#close-nav");
+  const closeNavBtn = document.querySelector("#close-nav-btn");
+  const titleLinks = document.querySelectorAll(".title-link");
+  const links = document.querySelectorAll(".links");
+  const navOpen = document.querySelectorAll(".nav-open");
+  const navDiv = document.querySelectorAll(".nav-div");
+  const nav = document.querySelector("nav");
+
+  closeNavBtn.addEventListener("click", () => {
+    closeNav.classList.add("hidden");
+  });
+
+  openNav.addEventListener("click", () => {
+    closeNav.classList.remove("hidden");
+  });
+
+  titleLinks.forEach((title, i) =>
+    title.addEventListener("click", () => {
+      links[i].classList.toggle("hidden");
+    })
+  );
+
+  navOpen.forEach((n, i) =>
+    n.addEventListener("mouseover", () => {
+      navDiv.forEach((n, i) => {
+        n.classList.add("hidden");
+      });
+
+      navDiv[i].classList.remove("hidden");
+    })
+  );
+
+  navDiv.forEach((n, i) =>
+    n.addEventListener("mouseleave", () => {
+      n.classList.add("hidden");
+    })
+  );
+
+  nav.addEventListener("mouseleave", () => {
+    navDiv.forEach(n => n.classList.add("hidden"));
+  });
+});
